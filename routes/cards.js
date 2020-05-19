9b81fd3c98f232cbaf1dfbf135d013df4135c7d4
Anton Vaitsehovskiy <antonvaits@gmail.com>
2020-05-19 11:46:18 +0300

    clean master

--- routes/cards.js
+++
@@ -1,8 +0,0 @@
-const routerCards = require('express').Router();
-const { createCard, getCards, cardRemove } = require('../controllers/cards');
-
-routerCards.delete('/:id', cardRemove);
-routerCards.get('/', getCards);
-routerCards.post('/', createCard);
-
-module.exports = routerCards;
