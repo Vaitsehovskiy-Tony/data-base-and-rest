de39a1d503746dcd691ce67943b94fe4db282195
Anton Vaitsehovskiy <antonvaits@gmail.com>
2020-05-23 05:00:54 +0300

    случайно смержил ветки. коммит после чистки мастераЭ

--- routes/users.js
+++
@@ -1,8 +0,0 @@
-const routerUsers = require('express').Router();
-const { getUsers, findUser } = require('../controllers/users');
-
-routerUsers.get('/', getUsers);
-// routerUsers.post('/', createUser);
-routerUsers.get('/:id', findUser);
-
-module.exports = routerUsers;
